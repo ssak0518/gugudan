73e61683ba1c07ac41d45cf3b4bd050988713f0f
simyj <simyj@172.30.1.2>
2021-07-05 15:01:57 +0900

    구구단

---
+++ src/Gugudan.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Gugudan {
+	public static void main(String[] args) {
+	
+	System.out.println("구구단중 출력한 단은? : ");
+	Scanner scanner = new Scanner(System.in);
+	int number = scanner.nextInt();
+	System.out.println(number * 1);
+	System.out.println(number * 2);
+	System.out.println(number * 3);
+	System.out.println(number * 4);
+	System.out.println(number * 5);
+	System.out.println(number * 6);
+	System.out.println(number * 7);
+	System.out.println(number * 8);
+	System.out.println(number * 9);
+	}
+}
